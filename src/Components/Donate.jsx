7e9a071b87e274235d50e9dbf4ac6f44e066a1e7
jenilparmar <jerryhash231@gmail.com>
2024-06-22 00:28:34 +0530

    Improved some of the styling of Main page and Home page

--- src/Components/Donate.jsx
+++ src/Components/Donate.jsx
@@ -1,23 +1,32 @@
-import React, { useContext } from 'react'
-import StateContext from './mycontext'
+import React, { useContext } from 'react';
+import StateContext from './mycontext';
 
 export default function Donate() {
-  const {setDonate} = useContext(StateContext);
+  const { setDonate } = useContext(StateContext);
+
   return (
-    <>
-     <center>
-     <div className="donate w-11/12 h-fit bg-slate-200 ">
-    <div className="text-5xl font-bold p-5 text-blue-400">Support Our Rescue Efforts</div>
-    <h2 className='font-semibold text-xl my-5 text-red-600'>Your donation can make a difference!</h2>
-    <p className='w-10/12 my-5'>Help us save more animals by making a donation today. Every contribution, big or small, helps us provide the care and support that injured and homeless animals need.</p>
-    <div className=" bg-blue-400 border-2 border-black hover:bg-blue-500 cursor-pointer self-center text-center my-10 font-semibold p-2 rounded-lg text-black w-40" onClick={()=>{
-      setDonate(false)
-    }}>
-         Donate
+    <div className="container mx-auto p-4">
+      <div className="donate bg-slate-200 rounded-lg shadow-md p-6">
+        <div className="text-3xl md:text-5xl font-bold text-blue-400 text-center mb-4">
+          Support Our Rescue Efforts
         </div>
-   
-     </div>
-     </center>
-    </>
-  )
+        <h2 className="font-semibold text-xl text-red-600 text-center mb-4">
+          Your donation can make a difference!
+        </h2>
+        <p className="text-center mb-6">
+          Help us save more animals by making a donation today. Every contribution, big or small, helps us provide the care and support that injured and homeless animals need.
+        </p>
+        <div className="flex justify-center">
+          <button
+            className="button bg-blue-400 hover:bg-blue-500 text-black font-semibold p-2 rounded-lg w-40"
+            onClick={() => {
+              setDonate(false);
+            }}
+          >
+            Donate
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,97 +3,77 @@ import StateContext from "./mycontext";
 import axios from "axios";
 
 export default function Main() {
-  const [goodHumen, setGoodHumen] = useState(0);
-  const [organizationCount,setOrganizationCount] = useState(0);
-  const [donate,setDonater] = useState(0);
-  function BackendSite(params) {
-    axios
-      .get("https://pawsraksha-1.onrender.com/ShowData")
-
-      .then((res) => setGoodHumen(res.data.length))
-      .catch((e) => {
-        console.log(e);
-      });
-
-    axios
-      .get("https://pawsraksha-1.onrender.com/GetOrganizations")
-      .then(res=>{
-        setOrganizationCount(res.data.length);
-      })
-      .catch(e=>{
-        console.log(e);
-      })
-  }
-  function LocalHost(params) {
-    axios
-      .get("https://pawsraksha-1.onrender.com/ShowData")
-      .then((res) => setGoodHumen(res.data.length))
-      .catch((e) => {
-        BackendSite();
-      });
-      axios
-      .get("https://pawsraksha-1.onrender.com/GetOrganizations")
-      .then(res=>{
-        setOrganizationCount(res.data.length);
-      })
-      .catch(e=>{
-        BackendSite();
-
-      })
-  }
+  const [goodHumans, setGoodHumans] = useState(0);
+  const [organizationCount, setOrganizationCount] = useState(0);
+  const [donate, setDonate] = useState(0);
 
   useEffect(() => {
-  LocalHost();
+    fetchData();
   }, []);
+
+  const fetchData = () => {
+    axios
+      .get("https://pawsraksha-1.onrender.com/ShowData")
+      .then((res) => setGoodHumans(res.data.length))
+      .catch(() => setGoodHumans(0)); // Handle error gracefully
+
+    axios
+      .get("https://pawsraksha-1.onrender.com/GetOrganizations")
+      .then((res) => setOrganizationCount(res.data.length))
+      .catch(() => setOrganizationCount(0)); // Handle error gracefully
+  };
+
   const { setIsRescue } = useContext(StateContext);
+
   return (
-    <>
-      <center>
-        <div className="main bg-slate-200 overflow-x-hidden  w-11/12 h-screen flex flex-col justify-center">
-          <div className="counts flex flex-row gap-3 justify-center w-fit p-6 self-center">
-            <div className="  w-36 h-40 flex flex-col justify-center">
-              <div className="text-8xl font-bold text-center self-center">
-                {goodHumen}
-              </div>
-              <div className="text-xl font-semibold text-center text-nowrap">
-                Good Humens
-              </div>
+    <div className="container mx-auto p-4 md:p-0">
+      <div className="text-center">
+        <div className="flex flex-col gap-6 md:flex-row md:justify-center">
+        <div className="w-80 h-40 flex flex-col justify-center bg-gray-200 rounded-lg shadow-md p-4">
+            <div className="text-4xl md:text-6xl font-bold text-center">
+            0
             </div>
-            <div className="  w-36 h-40 flex flex-col justify-center">
-              <div className="text-8xl font-bold text-center self-center">
-              {organizationCount}
-              </div>
-              <div className="text-xl font-semibold text-center text-nowrap">
-                Organizations
-              </div>
-            </div>
-            <div className="  w-36 h-40 flex flex-col justify-center">
-              <div className="text-8xl font-bold text-center self-center">
-               {donate}
-              </div>
-              <div className="text-xl font-semibold text-center text-nowrap">
-                Animals Rescued
-              </div>
+            <div className="text-xl md:text-2xl font-semibold text-center">
+              Animals Rescued
             </div>
           </div>
-          <h2 className="font-bold text-3xl p-3 text-blue-400">
-            Help Us Save Animals in Need
-          </h2>
-          <p className="p-3 font-medium">
-            If you've found an injured animal, you can help us by sharing a
-            photo. Click the button below to upload an image and provide
-            important details about the animal's condition and location. Your
-            contribution can make a huge difference!
-          </p>
-          <div
-            className=" button bg-blue-400 my-4 border-2 border-black hover:bg-blue-500 cursor-pointer self-center text-center font-semibold p-2 rounded-lg text-black w-40"
+          <div className="w-80 h-40 flex flex-col justify-center bg-gray-200 rounded-lg shadow-md p-4">
+            <div className="text-4xl md:text-6xl font-bold text-center">
+              {goodHumans}
+            </div>
+            <div className="text-xl md:text-2xl font-semibold text-center">
+            Superhero Users
+            </div>
+          </div>
+          <div className="w-80 h-40 flex flex-col justify-center bg-gray-200 rounded-lg shadow-md p-4">
+            <div className="text-4xl md:text-6xl font-bold text-center">
+              {organizationCount}
+            </div>
+            <div className="text-xl md:text-2xl font-semibold text-center">
+              Helping Organizations
+            </div>
+          </div>
+
+        </div>
+        <h2 className="font-bold text-3xl p-3 text-blue-400 mt-6">
+          Help Us Save Animals in Need
+        </h2>
+        <p className="p-3 font-medium text-center">
+          If you've found an injured animal, you can help us by sharing a photo.
+          Click the button below to upload an image and provide important
+          details about the animal's condition and location. Your contribution
+          can make a huge difference!
+        </p>
+        <div className="button bg-blue-400 my-4 border-2 border-black hover:bg-blue-500 cursor-pointer self-center text-center font-semibold p-2 rounded-lg text-black w-40 mx-auto">
+          <button
             onClick={() => {
               setIsRescue(true);
-            }}>
+            }}
+          >
             Rescue Animal
-          </div>
+          </button>
         </div>
-      </center>
-    </>
+      </div>
+    </div>
   );
 }
