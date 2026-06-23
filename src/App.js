c2d1e3830a3935158a08413dad37f942642619fc
Jenil <142485028+jenilparmar@users.noreply.github.com>
2026-06-24 00:12:27 +0530

    Wire ChatBot into App

--- src/App.js
+++ src/App.js
@@ -6,23 +6,20 @@ import AuthenticationPage from "./Components/AuthenticationPage";
 import NearestHelp from "./Components/NearestHelp";
 import MapRedirectButton from "./Components/MapRedirectButton";
 import Loader from "./Components/Loader";
+import ChatBot from "./Components/ChatBot";
 
 function App() {
-  // Initialize state, using localStorage if available
   const [isLogin, setIsLogin] = useState(false);
   const [isRescue, setIsRescue] = useState(false);
   const [donate, setDonate] = useState(true);
   const [Authenticated, setAuthenticated] = useState(false);
-  // cosnt [isAuth , setISAuth] = useState(false);
 
   useEffect(() => {
-    // Check if there's a saved authentication state in localStorage
     const savedAuthenticated = localStorage.getItem("Authenticated");
     if (savedAuthenticated) {
       setAuthenticated(true);
     }
   }, []);
-
 
   useEffect(()=>{
     if(isLogin)  localStorage.setItem("Authenticated", JSON.stringify(true));
@@ -34,11 +31,8 @@ function App() {
       <StateContext.Provider
         value={{ setIsLogin, setAuthenticated, isRescue, isLogin, setIsRescue, setDonate, donate }}
       >
-        {/* {!false ? <AuthenticationPage /> : <Home  />} */}
         {!Authenticated ? <AuthenticationPage /> : <Home  />}
-        {/* <Loader/> */}
-        {/* <NearestHelp/> */}
-        {/* <MapRedirectButton/> */}
+        {Authenticated && <ChatBot />}
       </StateContext.Provider>
     </> 
   );
